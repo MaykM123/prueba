011589ce4374cb50e9c97c950e2be9708fa199e3
Maykol Méndez <maykolmendez4002@gmail.com>
2025-05-23 22:23:06 -0400

    Unef

---
+++ Fibonacci.py
@@ -0,0 +1,17 @@
+
+
+iniFibo = 0
+finFibo = 1
+x = 1
+while True:
+    
+    if x == 0:
+        finFibo = 0
+    elif x == 1:
+        finFibo = 1
+
+    sum = finFibo
+    finFibo = finFibo + iniFibo
+    iniFibo = finFibo - iniFibo
+
+print(f'\nPosición Final:{x+1} -- Valor en la serie de Fibonacci:{sum}')
